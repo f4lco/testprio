2b8e078eccbd0941cac5fc282f6482b938ec0b5a
Falco Duersch <falco.duersch@student.hpi.de>
2019-02-01 16:57:52 +0100

    Clean up Gradle configs and configure shadow plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,16 @@
  */
 
 rootProject.name = "testprio"
+
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "kotlin-multiplatform") {
+                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+            }
+            if (requested.id.id == "kotlinx-serialization") {
+                useModule("org.jetbrains.kotlin:kotlin-serialization:${requested.version}")
+            }
+        }
+    }
+}
