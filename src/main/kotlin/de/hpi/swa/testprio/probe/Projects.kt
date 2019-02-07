79c528779d0cdb750aa3d798c12f7aa98a7340ba
Falco Duersch <falco.duersch@student.hpi.de>
2019-02-07 18:55:59 +0100

    Replace query for jobs with test failures with more accurate version
    The previous query version relies on the log file analysis
    of TravisTorrent to find red tests. It was likely that our
    build log analyzer and TT find the same candidate jobs.
    
    However, TT has no parser for Buck builds. For example project
    facebook/buck no red tests are recorded in TT at all.
    Therefore it is mandatory at least for all non-Maven builds
    to join with the results of our log analysis.

--- src/main/kotlin/de/hpi/swa/testprio/probe/Projects.kt
+++ src/main/kotlin/de/hpi/swa/testprio/probe/Projects.kt
@@ -4,6 +4,7 @@ import org.jooq.DSLContext
 import org.jooq.impl.DSL.field
 import org.jooq.impl.DSL.name
 import org.jooq.impl.DSL.table
+import org.jooq.impl.DSL.selectOne
 
 object Projects {
 
@@ -15,10 +16,12 @@ object Projects {
                     .fetch(0, String::class.java)
 
     fun redJobIdsOf(context: DSLContext, projectName: String) =
-            context.select(field(name("tr_job_id")))
-                    .from(table(name("travistorrent_8_2_2017")))
-                    .where(field(name("gh_project_name")).eq(projectName))
-                    .and(field(name("tr_log_bool_tests_ran")).isTrue)
-                    .and(field(name("tr_log_bool_tests_failed")).isTrue)
-                    .fetch(0, String::class.java)
+        context.select(field(name("tt", "tr_job_id")))
+                .from(table(name("travistorrent_8_2_2017")).`as`("tt"))
+                .where(field(name("tt", "gh_project_name")).eq(projectName))
+                .andExists(
+                        selectOne().from(table(name("tr_test_result")).`as`("tr"))
+                        .where("tr.tr_job_id = tt.tr_job_id")
+                        .and(field(name("tr", "failures")).gt(0).or(field(name("tr", "errors")).gt(0)))
+                ).fetch(0, String::class.java)
 }
