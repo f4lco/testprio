c98e69c0c9e2aa984f62eb7501ca53868ff0d154
Falco Duersch <falco.duersch@student.hpi.de>
2019-02-12 10:20:17 +0100

    Consistently name experiment scripts + add matrix alpha experiment

---
+++ bin/matrix-alpha.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source .testprio
+
+seq 0 0.1 1 | xargs -t -Ivalue java -jar $PRIO_JAR \
+  matrix \
+  --host localhost \
+  --port 5432 \
+  --user ma \
+  --db github \
+  --project "square/okhttp" \
+  --alpha value \
+  --window -1 \
+  --output square-okhttp-matrix-alpha-value.csv
