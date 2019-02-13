488100a3757ac75d092b1d11cca7baab4d0432ad
Falco Duersch <falco.duersch@student.hpi.de>
2019-02-13 10:27:52 +0100

    Accept script parameters, add result folder in baseline script

--- bin/baseline.sh
+++ bin/baseline.sh
@@ -3,11 +3,11 @@ set -eu
 
 . .testprio
 
-project=facebook/buck
-slug=buck
+project=$1
+slug=$2
 
 function run_strategy() {
-  java -jar ${PRIO_JAR} $1 --user ma --project ${project} --output ${slug}-$1.csv
+  java -jar ${PRIO_JAR} $1 --user ma --project ${project} --output results/${slug}-$1.csv
 }
 
 for strategy in untreated random lru recently-failed matrix matrix-similarity
