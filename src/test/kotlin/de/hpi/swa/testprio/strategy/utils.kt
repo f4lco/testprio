bcef99c627110eb2c183a050eedadde5dcc13eb8
Falco Duersch <falco.duersch@student.hpi.de>
2019-02-19 11:29:39 +0100

    Add printing routine for Matrix to test utils

--- src/test/kotlin/de/hpi/swa/testprio/strategy/utils.kt
+++ src/test/kotlin/de/hpi/swa/testprio/strategy/utils.kt
@@ -1,5 +1,7 @@
 
 import de.hpi.swa.testprio.parser.TestResult
+import de.hpi.swa.testprio.strategy.matrix.Key
+import de.hpi.swa.testprio.strategy.matrix.Matrix
 import strikt.api.Assertion
 import strikt.assertions.containsExactly
 import strikt.assertions.map
@@ -10,3 +12,27 @@ fun <T : Iterable<TestResult>> Assertion.Builder<T>.hasTestOrder(vararg names: S
             } then {
                 if (allPassed) pass() else fail()
             }
+
+fun Matrix.print() {
+    print(tableString())
+}
+
+fun Matrix.tableString(n: Int = 4): String = StringBuilder(128).apply {
+    val tc = matrix.keys.map { it.testName }.toSortedSet()
+
+    append(" ".repeat(n))
+    for (name in tc) {
+        append(name.padEnd(n))
+    }
+    appendln()
+
+    val files = matrix.keys.map { it.fileName }.toSortedSet()
+    for (name in files) {
+        append(name.padEnd(n))
+        for (t in tc) {
+            val value = matrix[Key(name, t)] ?: 0
+            append(value.toString().padEnd(n))
+        }
+        appendln()
+    }
+}.toString()
