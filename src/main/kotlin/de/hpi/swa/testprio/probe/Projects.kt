d8f1fc838a931e1a6c30068a5701abb99160b50f
Falco Duersch <falco.duersch@student.hpi.de>
2019-03-04 13:24:13 +0100

    Bugfix offending jobs query

--- src/main/kotlin/de/hpi/swa/testprio/probe/Projects.kt
+++ src/main/kotlin/de/hpi/swa/testprio/probe/Projects.kt
@@ -26,21 +26,23 @@ object Projects {
                 ).fetch(0, String::class.java)
 
     fun firstFailuresOf(context: DSLContext, projectName: String): List<String> =
-            context.select(field(name("tt1", "tr_job_id")))
-                .from(
-                    table(name("travistorrent_8_2_2017")).`as`("tt1"),
-                    table(name("travistorrent_8_2_2017")).`as`("tt2")
-                ).where(
-                    field(name("tt1", "gh_project_name")).eq(projectName)
-                    .and(field(name("tt2", "gh_project_name")).eq(projectName))
-                    .and(field(name("tt1", "tr_build_number")).minus(1).eq("tt2.tr_build_number"))
-                    .andExists(
+        context.select(field(name("tt1", "tr_job_id")))
+            .from(table(name("travistorrent_8_2_2017")).`as`("tt1"))
+            .where(
+                field(name("tt1", "gh_project_name")).eq(projectName)
+                .andExists(
                         selectOne().from(table(name("tr_test_result")).`as`("tr1"))
-                        .where("tr1.tr_job_id = tt1.tr_job_id")
-                        .and(field(name("tr1", "failures")).gt(0).or(field(name("tr1", "errors")).gt(0)))
-                    ).andNotExists(
-                        selectOne().from(table(name("tr_test_result")).`as`("tr2"))
-                        .where("tr2.tr_job_id = tt2.tr_job_id")
-                        .and(field(name("tr2", "failures")).gt(0).or(field(name("tr2", "errors")).gt(0))))
-                ).fetch(0, String::class.java)
+                            .where("tr1.tr_job_id = tt1.tr_job_id")
+                            .and(field(name("tr1", "failures")).gt(0).or(field(name("tr1", "errors")).gt(0)))
+                )
+                .andNotExists(
+                    selectOne().from(table(name("travistorrent_8_2_2017")).`as`("tt2"))
+                        .where("tt2.gh_project_name = tt1.gh_project_name").and("tt1.tr_build_number = tt2.tr_build_number + 1")
+                        .andNotExists(
+                            selectOne().from(table(name("tr_test_result")).`as`("tr2"))
+                                .where("tr2.tr_job_id = tt2.tr_job_id")
+                                .and(field(name("tr2", "failures")).gt(0).or(field(name("tr2", "errors")).gt(0)))
+                        )
+                )
+            ).fetch(0, String::class.java)
 }
