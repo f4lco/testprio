f90c0fbf4fcf90fb9c4c8c25a84d8ff9f7b06be6
Falco Duersch <falco.duersch@student.hpi.de>
2019-03-07 12:50:02 +0100

    Improve Maven log parser suite

--- src/test/kotlin/de/hpi/swa/testprio/parser/MavenLogParserTest.kt
+++ src/test/kotlin/de/hpi/swa/testprio/parser/MavenLogParserTest.kt
@@ -3,7 +3,8 @@ package de.hpi.swa.testprio.parser
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
 import strikt.assertions.filter
-import strikt.assertions.isNotEmpty
+import strikt.assertions.hasSize
+import strikt.assertions.isEmpty
 import java.io.File
 
 class MavenLogParserTest {
@@ -13,8 +14,28 @@ class MavenLogParserTest {
         val result = MavenLogParser.parseFile(find("6756_5b0897af57ca7765631a2e86da1fcf9d137d9adc_116837515.log"))
 
         expectThat(result.toList()) {
-            isNotEmpty()
-            filter { it.red > 0 }.isNotEmpty()
+            hasSize(139)
+            filter { it.red > 0 }.hasSize(1)
+        }
+    }
+
+    @Test
+    fun testResultAndNameInOneLine() {
+        val result = MavenLogParser.parseFile(find("3710_6b6f5f917f6caf170136d5fdf9d022e1ecc37470_78417927.log"))
+
+        expectThat(result.toList()) {
+            hasSize(288)
+            filter { it.red > 0 }.isEmpty()
+        }
+    }
+
+    @Test
+    fun testDurationParsingOnInterleavedTestResults() {
+        val result = MavenLogParser.parseFile(find("5176_eee39ea7a547cfc11707e0c62ff558fb97e53c22_108786409.log"))
+
+        expectThat(result.toList()) {
+            hasSize(305)
+            filter { it.red > 0 }.isEmpty()
         }
     }
 
